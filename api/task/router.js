732961cad0f31dbfcaca2b3fbe9da8a276c0e614
Austin Carman <abcarman12@gmail.com>
2021-06-11 10:19:08 -0600

    tasks router is working

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,20 @@
-// build your `/api/tasks` router here
+const express = require('express');
+const taskRouter = express.Router();
+
+
+taskRouter.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'taskRouter is working'
+    });
+});
+
+taskRouter.use((err, req, res, next) => {
+    res.json({
+        status: 500, 
+        message: err.message,
+        stack: err.stack
+    });
+});
+
+
+module.exports = taskRouter;
