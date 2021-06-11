6abea03a8c7026741206e4aae8ca8edb5e9f400a
Austin Carman <abcarman12@gmail.com>
2021-06-11 12:44:41 -0600

    get endpoint working for projects

--- api/project/model.js
+++ api/project/model.js
@@ -1,5 +1,17 @@
-function getProjects() {
-    return Promise.resolve('retrieved all projects');
+const db = require('../../data/dbConfig');
+
+
+async function getProjects() {
+    const rows = await db('projects')
+    const projects = rows.map(project => {
+            if (project.project_completed === 1) {
+                return {...project, project_completed: true}
+            } else {
+                return {...project, project_completed: false}
+            }
+    });
+    return projects;
 }
+
 
 module.exports = { getProjects }
