8fcc8b9339112a791ef606941652fe069c016619
Austin Carman <abcarman12@gmail.com>
2021-06-11 11:14:06 -0600

    projects table and resources table working

--- data/migrations/20210611163940_initial-migration.js
+++ data/migrations/20210611163940_initial-migration.js
@@ -2,10 +2,16 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('projects', table => {
-        table.increments()
+        table.increments('project_id')
+        table.string('project_name').notNullable()
+        table.string('project_description')
+        table.boolean('project_completed').defaultTo(0)
+        
     })
     .createTable('resources', table => {
-        table.increments()
+        table.increments('resource_id')
+        table.string('resource_name').notNullable()
+        table.string('resource_description')
     })
     .createTable('tasks', table => {
         table.increments()
