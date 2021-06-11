4ef36295c33d664221c820ef675f7c8b622cafb8
Austin Carman <abcarman12@gmail.com>
2021-06-11 10:09:37 -0600

    project router is connected

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,21 @@
 // build your `/api/projects` router here
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'projects api working'
+    });
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage: 'something in the app is not working',
+        message: err.message,
+        stack: err.stack,
+    })
+});
+
+
+module.exports = router;
